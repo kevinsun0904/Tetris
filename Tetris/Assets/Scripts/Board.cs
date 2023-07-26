908732747d408f6ce35e2a72d8b32e652d1170c2
kevinsun0904 <122562552+kevinsun0904@users.noreply.github.com>
2023-07-25 20:32:57 -0700

    created piece script and edited board script
    i love you vivs

--- Tetris/Assets/Scripts/Board.cs
+++ Tetris/Assets/Scripts/Board.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour {
+    public Tilemap tilemap { get; private set; }
     public TetrominoData[] tetrominos;
-    
+
+    private void Awake() {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+
+        for (int i = 0; i < this.tetrominos.Length; i++) {
+            this.tetrominos[i].Initialize();
+        }
+    }
+
+    private void Start() {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece() {
+        int random = Random.Range(0, this.tetrominos.Length);
+        TetrominoData data = this.tetrominos[random];
+    }
+
+    public void Set() {
+
+    }
 }
