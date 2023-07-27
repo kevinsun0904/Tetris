0a7700ff012f205ef6c26b3186e6deabed5ccb18
kevinsun0904 <122562552+kevinsun0904@users.noreply.github.com>
2023-07-27 15:27:56 -0700

    completed spawn piece
    This part can be referred to when implementing hold in the future.
    
    I miss u so much vivs

--- Tetris/Assets/Scripts/Board.cs
+++ Tetris/Assets/Scripts/Board.cs
@@ -3,10 +3,13 @@ using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour {
     public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominos;
+    public Vector3Int spawnPosition; 
 
     private void Awake() {
         this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
 
         for (int i = 0; i < this.tetrominos.Length; i++) {
             this.tetrominos[i].Initialize();
@@ -20,9 +23,15 @@ public class Board : MonoBehaviour {
     public void SpawnPiece() {
         int random = Random.Range(0, this.tetrominos.Length);
         TetrominoData data = this.tetrominos[random];
+
+        this.activePiece.Initialize(this, this.spawnPosition, data);
+        Set(activePiece);
     }
 
-    public void Set() {
-
+    public void Set(Piece piece) {
+        for (int i = 0; i < piece.cells.Length; i++) {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tilemap.SetTile(tilePosition, piece.data.tile);
+        }
     }
 }
