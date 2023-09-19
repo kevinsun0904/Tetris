78d54cb76ce3b3cba2d46206013d468d201f4cef
Vivian <118199397+vivjd@users.noreply.github.com>
2023-09-18 23:45:57 -0400

    Update Score.cs

--- Tetris/Assets/Scripts/Score.cs
+++ Tetris/Assets/Scripts/Score.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Score : MonoBehaviour
 {
+    public Board board;
+    public Tile tile { get; private set; }
+    public Tilemap tilemap { get; private set; }
+    public int CurrentScore;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        CurrentScore = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CalculateScore(){
+
     }
 }
